eceaf0c63a150e8aae9477d0420606b3a76281fa
Oran C <oranbusiness@gmail.com>
2018-01-03 12:24:33 -0800

    added inital file with comment

---
+++ setup.js
@@ -0,0 +1,14 @@
+/*
+p5.js setup application
+to insitalize a project without messing 
+with terminal for each project
+AKA make it easy to setup & run  p5.js  projects
+from the terminal
+
+by wisemonkey
+180103
+*/
+
+require("wise-helper");
+
+
